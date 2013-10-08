b6dd9a8e87571a66f061c771aa934472167fee97
Jorge Reyero <jorgereyero@gmail.com>
2013-10-08 21:21:42 +0100

    Initial Owin and Nancy setup

---
+++ NancyBugTracker/App_Start/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace NancyBugTracker.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
---
+++ NancyBugTracker/HomeModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace NancyBugTracker
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World Nancy";
+
+        }
+    }
+}
